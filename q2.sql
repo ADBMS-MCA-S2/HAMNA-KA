db626acc95fd4e8ba63a7eb4d380e5e63e932406
Hamna K A <88711212+HamnaKA@users.noreply.github.com>
2022-03-23 15:30:49 +0530

    Create q2.sql

---
+++ q2.sql
@@ -0,0 +1,10 @@
+create database depat;
+use depat;
+create table dept(deptno int(50) primary key, dname varchar(50),LOC varchar(50));
+alter table dept ADD pincode int(50) NOT NULL;
+select * from  dept;
+ALTER TABLE dept CHANGE dname dept_name varchar(15);
+ALTER TABLE dept MODIFY COLUMN LOC varchar(10); 
+ALTER TABLE dept drop PRIMARY KEY;
+RENAME TABLE dept TO department;
+drop table department;
