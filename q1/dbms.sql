753bfb70f2fef39e90cf2d66d28954d9dddb6172
Hamna K A <88711212+HamnaKA@users.noreply.github.com>
2022-05-03 22:07:11 +0530

    Update dbms.sql

--- q1/dbms.sql
+++ q1/dbms.sql
@@ -1,67 +1,33 @@
-create database movie;
-use movie;
-CREATE TABLE acts(
-    actor varchar(255)NOT NULL,
-    title varchar(255)NOT NULL,
-     FOREIGN KEY (actor) REFERENCES actors(actor),
-    FOREIGN KEY (title) REFERENCES movies(title)
-    
-    );
-      INSERT INTO acts (actor,title)
-VALUES ('cage','raising arizona'),
-('maguire','spiderman'),
-('maguire','wonder boys'),
-('mcdarmand','fargo'),
-('mcdarmand','raising arizona'),
-('mcdarmand','wonder boys');
-    
-    select * from acts;
-CREATE TABLE actors(
-    actor varchar(255)NOT NULL,
-    ayear varchar(255)NOT NULL,
-    
-   primary key(actor)
-    );
-    INSERT INTO actors (actor,ayear)
-VALUES ('cage',1964),
-('hanks',1956),
-('maguire',1975),
-('mcdarmand',1957);
+create database moviedatabase;
+use moviedatabase;
+create table movies(title varchar(30) not null,director varchar(20) not null,myear int(5),rate float(3),primary key(title));
+insert into movies values("Fargo","Coen",1996,8.2);
+insert into movies values("Raising Arizona","Coen",1987,7.6);
+insert into movies values("Spiderman","Raimi",2002,7.4);
+insert into movies values("Wonder Boys","Hanson",2000,7.6);
+create table actors(actor varchar(20) not null, ayear int(5) not null,primary key(actor));
+insert into actors values("Cage",1964);
+insert into actors values("Hanks",1956);
+insert into actors values("Maguire",1975);
+insert into actors values("McDormand",1957);
+create table directors(director varchar(20) not null,dyear int(5) not null,primary key(director));
+insert into directors values("Coen",1954);
+insert into actors values("Hanson",1945);
+insert into actors values("Raimi",1959);
+create table acts(actor varchar(20) not null,title varchar(30) not null,foreign key(actor) references actors (actor) on delete cascade,foreign key(title) references movies (title) on delete cascade);
+insert into acts values("Cage","Raising Arizona");
+insert into acts values("Maguire","Spiderman");
+insert into acts values("Maguire","Wonder Boys");
+insert into acts values("McDormand","Fargo");
+insert into acts values("McDormand","Raising Arizona");
+insert into acts values("McDormand","Wonder Boys");
 
 
-CREATE TABLE movies(
-    title varchar(255)NOT NULL,
-    director varchar(255)NOT NULL,
-    myear varchar(255)NOT NULL,
-    rating varchar(255)NOT NULL,
-    primary key(title),
-    FOREIGN KEY (director) REFERENCES directors(director)
-    );
-    INSERT INTO movies (title,director,myear,rating)
-VALUES ('fargo','coen',1996,8.2),
-('raising arizona','coen',1987,7.6),
-('spiderman','raimi',2002,7.4),
-('wonder boys','hanson',2000,7.6);
-
-CREATE TABLE directors(
-    director varchar(255)NOT NULL,
-    dyear varchar(255)NOT NULL,
-    primary key(director)
-    );
-INSERT INTO directors (director, dyear)
-VALUES ('coen',1954),
-('hanson',1945),
-('raimi',1959);
- 
- select title from  movies where (myear>1997);
- SELECT title FROM movies
-WHERE myear>1997 AND director='hanson';
-select title,rating from movies;
-
-SELECT title FROM acts WHERE actor = 'mcdarmand'AND title in (select title from movies where director='coen');
-SELECT actor as 'Actor' FROM Actors
-UNION
-SELECT director as 'Actor' FROM Directors;
+select * from movies where myear>1997; 
+select *from movies where director="Hanson" and myear>1997; 
+select title,rate from movies;  
+select movies.director,acts.actor from movies  inner join acts on movies.title=acts.title; 
+select movies.title from movies inner join acts on movies.title=acts.title where director="Coen" and actor="McDormand";  
 
 
  
